1183afe33c6c198612a9dff97c8e1964e6e3a954
Yoser Perez <yoserp1@gmail.com>
2021-05-20 14:15:47 -0500

    Servicio para detectar estatus de red

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+import { ConexionService } from '../services/conexion.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    public alerta: AlertController,
+    private conexion: ConexionService
+  ) {}
+
+  async ngOnInit() {
+
+    this.conexion.appIsOnline$.subscribe(online => {
+      //console.log(online);
+      if (online) {
+          this.conServicio();
+          //console.log('Hay internet');
+      } else {
+          this.sinServicio();
+          //console.log('Sin internet');
+      }
+    });
+
+  }
+
+  async conServicio() {
+    const mensaje = await this.alerta.create({
+      header: 'Mensaje',
+      message: 'Conectado!',
+      buttons: ['OK']
+    });
+    await mensaje.present();
+    const { role } = await mensaje.onDidDismiss();
+  }
+
+  async sinServicio() {
+    const mensaje = await this.alerta.create({
+      header: 'Mensaje',
+      message: 'Desconectado!',
+      buttons: ['OK']
+    });
+    await mensaje.present();
+    const { role } = await mensaje.onDidDismiss();
+  }
 
 }
