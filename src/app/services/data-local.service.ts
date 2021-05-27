51066bcd5d8cbf655cedfb7dfcffe9b1c8d73230
Yoser Perez <yoserp1@gmail.com>
2021-05-26 22:57:45 -0500

    componentes para manejo de storage

---
+++ src/app/services/data-local.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataLocalService {
+  private _storage: Storage | null = null;
+
+  constructor( private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    // eslint-disable-next-line no-underscore-dangle
+    this._storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._storage?.set(key, value);
+  }
+}
