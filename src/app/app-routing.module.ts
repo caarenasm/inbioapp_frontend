5d0c18116f01edfcca469d55c0ce0d09502bdad0
Carlos Andres Arenas Mejia <andresam98@hotmail.com>
2021-05-27 16:59:41 -0500

    Pagina quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,15 @@ const routes: Routes = [
   {
     path: 'quiz',
     loadChildren: () => import('./pages/quiz/quiz.module').then( m => m.QuizPageModule)
+  },  {
+    path: 'objetivos',
+    loadChildren: () => import('./pages/objetivos/objetivos.module').then( m => m.ObjetivosPageModule)
   },
+  {
+    path: 'plan',
+    loadChildren: () => import('./pages/plan/plan.module').then( m => m.PlanPageModule)
+  },
+
 
 ];
 
