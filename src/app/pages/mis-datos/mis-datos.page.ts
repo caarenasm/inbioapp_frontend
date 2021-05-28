6f4ef952acd0ffbf821c16a38a7277acabbe9814
Yoser Perez <yoserp1@gmail.com>
2021-05-28 09:17:02 -0500

    Arreglo de pagina de detalle de datos

--- src/app/pages/mis-datos/mis-datos.page.ts
+++ src/app/pages/mis-datos/mis-datos.page.ts
@@ -2,6 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MisDatosDetallePage } from '../mis-datos-detalle/mis-datos-detalle.page';
 
+export interface Datos {
+  genero: string;
+  fechaNac: string;
+  estatura: string;
+  miObjetivo: string;
+  pesoActual: string;
+  pesoDeseado: string;
+  imc: string;
+  tdee: string;
+  pcg: string;
+  miPlan: string;
+}
+
+export interface Consulta {
+  descripcion: string; 
+  icon: string; 
+  respuesta: string; 
+  resultado: string[];
+}
+
 @Component({
   selector: 'app-mis-datos',
   templateUrl: './mis-datos.page.html',
@@ -9,11 +29,9 @@ import { MisDatosDetallePage } from '../mis-datos-detalle/mis-datos-detalle.page
 })
 export class MisDatosPage implements OnInit {
 
-  datos : { 
-    genero: string, fechaNac: string, estatura: string, miObjetivo: string, pesoActual: string,
-    pesoDeseado: string, imc: string, tdee: string, pcg: string,
-    miPlan: string
-  } = {
+  estrella = 'star-outline';
+
+  datos : Datos = {
       genero: 'Masculino',
       fechaNac: '12.10.1978',
       estatura: '1.65',
@@ -26,61 +44,65 @@ export class MisDatosPage implements OnInit {
       miPlan: 'Plan Saludable'
   };
 
-  consultas: { descripcion: string, icon: string, respuesta: string, resultado: string/*, resultado: any[]*/ }[] = [
+  consultas: Consulta[] = [
     {
       descripcion: 'Estado del Sueño',
       icon: 'add',
       respuesta: 'Duermo muy bien y me levanto descansado',
-      resultado: 'Bien por ti! Esto demuestra que la hormona del sueño está funcionando bien. Tu nutrición esta siendo adecuada para proveerte una calidad del sueño. Durante el sueño el organismo se regenera, se estimula el buen funcionamiento de los sistemas inmunitario y hormonal.'
+      resultado: [
+        'Bien por ti! Esto demuestra que la hormona del sueño está funcionando bien.',
+        'Tu nutrición esta siendo adecuada para proveerte una calidad del sueño.',
+        'Durante el sueño el organismo se regenera, se estimula el buen funcionamiento de los sistemas inmunitario y hormonal.'
+      ]
       //resultado:[ 'Estado del Sueño','concat']
     },
     {
       descripcion: 'Antojos',
       icon: 'add',
       respuesta: 'Todos los días',
-      resultado: 'Oh No! Muchos tipos de antojos están relacionados con la parte psicológica: las emociones y el deseo, por ejemplo para calmar el estrés y reducir la.'
+      resultado: ['Oh No! Muchos tipos de antojos están relacionados con la parte psicológica: las emociones y el deseo, por ejemplo para calmar el estrés y reducir la.']
     },
     {
       descripcion: 'Antojos más Comunes',
       icon: 'add',
       respuesta: 'Comidas con carbohidratos',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Actividades Físicas',
       icon: 'add',
       respuesta: 'Me siento cómodo(a) con las dos actividades físicas anteriores',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Enfermedades',
       icon: 'add',
       respuesta: 'Enfermedades cardiovasculares',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Dieta Especial',
       icon: 'add',
       respuesta: 'No, como casi de todo',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Desposiciones',
       icon: 'add',
       respuesta: '1 vez al día',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Consumo de Agua',
       icon: 'add',
       respuesta: 'De 1L a 2L',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     },
     {
       descripcion: 'Suplementos',
       icon: 'add',
       respuesta: 'Bioproductos Inbionova',
-      resultado: 'Oh No!'
+      resultado: ['Oh No!']
     }
   ];
 
@@ -91,7 +113,7 @@ export class MisDatosPage implements OnInit {
 
   async mostrarDetalle( datos?) {
 
-    console.log(datos);
+    //console.log(datos);
 
     const modal = await this.modalCtrl.create({
       component: MisDatosDetallePage,
@@ -107,6 +129,10 @@ export class MisDatosPage implements OnInit {
     //console.log('onWillDismiss');
     //console.log(data);
 
+  }
+
+  favorito() {
+    this.estrella = 'star';
   }
 
 }
