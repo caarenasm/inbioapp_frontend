9cc82da334d60f77fbeb8474df850252b6172e2b
Yoser Perez <yoserp1@gmail.com>
2021-05-28 09:13:51 -0500

    Arreglo de pagina de registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
+  estaturaActual = 0;
+  pesoActual = 0;
+  pesoDeseado = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Seleccionar',
+        handler: ( event ) => {
+          console.log(event);
+        }
+      },
+      {
+        text: 'Cancelar',
+        handler: () => {
+          console.log('log!')
+        }
+      },
+    ]
+  }
+
+  masEstatura() {
+    this.estaturaActual += 1;
+  }
+
+  menosEstatura() {
+    this.estaturaActual -= 1;
+  }
+
+  masPeso() {
+    this.pesoActual += 1;
+  }
+
+  menosPeso() {
+    this.pesoActual -= 1;
+  }
+
+  masPesoDeseado() {
+    this.pesoDeseado += 1;
+  }
+
+  menosPesoDeseado() {
+    this.pesoDeseado -= 1;
   }
 
 }
