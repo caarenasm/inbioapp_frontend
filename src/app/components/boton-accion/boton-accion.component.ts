d91bcdc4ff53a65ad684040e0aa2533163526b20
Carlos Andres Arenas Mejia <andresam98@hotmail.com>
2021-06-08 11:29:05 -0500

    boton reutilizable y mensajer alert

---
+++ src/app/components/boton-accion/boton-accion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-accion',
+  templateUrl: './boton-accion.component.html',
+  styleUrls: ['./boton-accion.component.scss'],
+})
+export class BotonAccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
