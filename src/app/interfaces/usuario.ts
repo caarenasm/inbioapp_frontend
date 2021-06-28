3726e739de3cf7d1920d6f88bea260a32a89f5b7
Carlos Andres Arenas Mejia <andresam98@hotmail.com>
2021-06-28 09:52:04 -0500

    api interfaz

---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+    sexo: number;
+    dia: string;
+    mes: string;
+    anio: string;
+    nombre: string;
+    apellido: string;
+    email: string;
+    estatura: number;
+    peso_actual: number;
+    peso_deseado: number;
+}
