3fb35a23f379fbddcc0d153832840f6851c2c23d
Carlos Andres Arenas Mejia <andresam98@hotmail.com>
2021-07-26 07:40:34 -0500

    Avance de la tarea 354 con calendario

---
+++ src/app/pages/mi-diario-nutricional/mi-diario-nutricional.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-diario-nutricional',
+  templateUrl: './mi-diario-nutricional.page.html',
+  styleUrls: ['./mi-diario-nutricional.page.scss'],
+})
+export class MiDiarioNutricionalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
