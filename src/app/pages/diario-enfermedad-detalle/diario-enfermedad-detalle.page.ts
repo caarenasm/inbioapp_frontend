9afa1f479cb2e4e91f44abbef5163f5488049773
Yoser Perez <yoserp1@gmail.com>
2021-08-06 09:32:24 -0500

    Formulario Diario Enfermedad

--- src/app/pages/diario-enfermedad-detalle/diario-enfermedad-detalle.page.ts
+++ src/app/pages/diario-enfermedad-detalle/diario-enfermedad-detalle.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-diario-enfermedad-detalle',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiarioEnfermedadDetallePage implements OnInit {
 
-  constructor() { }
+  datos: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertServ: AlertService,
+    private alertCtrl: AlertController,
+  ) {
+    this.datos = this.formBuilder.group({
+      tipo: [ 4, Validators.required],
+      medicamento: ['', Validators.required],
+      lectura: ['', ],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  guardar(){
+    if ( this.datos.invalid ){
+      this.alertServ.presentAlert('Datos Requeridos, verifique el formulario!');
+
+      return Object.values( this.datos.controls ).forEach( control => {
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched() );
+        } else {
+          control.markAsTouched();
+        }
+      });
+
+    }
+    this.confirmar();
+  }
+
+  async confirmar() {
+
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      message: '¿Estas seguro que deseas Guardar?',
+      cssClass:'alerta',
+      buttons: [
+        {
+          text: 'Volver',
+          role: 'cancel',
+        },
+        {
+          text: 'Continuar',
+          cssClass:'alerta-boton-aceptar',
+          handler: () => {
+            this.datos.get('lectura').setValue(JSON.stringify(this.datos.value));
+            console.log(this.datos.value);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
   }
 
 }
