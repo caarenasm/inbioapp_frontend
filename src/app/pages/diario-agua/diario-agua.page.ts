da56aeb8081f5f274e6dce2d09a017845b3f3dc5
Yoser Perez <yoserp1@gmail.com>
2021-09-30 13:36:03 -0500

    correciones en vistas

--- src/app/pages/diario-agua/diario-agua.page.ts
+++ src/app/pages/diario-agua/diario-agua.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides } from '@ionic/angular';
+import { AlertController, IonSlides, ModalController } from '@ionic/angular';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AlertService } from '../../services/alert.service';
 
 export interface Evento {
   id: number;
@@ -15,6 +18,7 @@ export interface Evento {
 })
 export class DiarioAguaPage implements OnInit {
 
+datos: FormGroup;
 vaso = 0;
 menosVasoDisabled = false;
 
@@ -37,8 +41,17 @@ evento: Evento[] = [
 ];
 
 constructor(
+  private formBuilder: FormBuilder,
+  private alertServ: AlertService,
+  private alertCtrl: AlertController,
+  private modalCtrl: ModalController,
   private location: Location
-) { }
+) { 
+  this.datos = this.formBuilder.group({
+    tipo: [ 1, Validators.required],
+    opcion: ['', Validators.required],
+  });
+}
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
   @ViewChild('miOpcion') slides: IonSlides;
@@ -71,6 +84,48 @@ constructor(
 
   cerrar() {
     this.location.back();
+  }
+
+  guardar(){
+    if ( this.datos.invalid ){
+      this.alertServ.presentAlert('Datos Requeridos, verifique el formulario!');
+
+      return Object.values( this.datos.controls ).forEach( cntrl => {
+        if ( cntrl instanceof FormGroup ) {
+          Object.values( cntrl.controls ).forEach( control => control.markAsTouched() );
+        } else {
+          cntrl.markAsTouched();
+        }
+      });
+
+    }
+    this.confirmar();
+  }
+
+  async confirmar() {
+
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      message: '¿Estas seguro que deseas Guardar?',
+      cssClass:'alerta',
+      buttons: [
+        {
+          text: 'Volver',
+          role: 'cancel',
+        },
+        {
+          text: 'Continuar',
+          cssClass:'alerta-boton-aceptar',
+          handler: () => {
+            this.datos.get('lectura').setValue(JSON.stringify(this.datos.value));
+            console.log(this.datos.value);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
   }
 
 }
