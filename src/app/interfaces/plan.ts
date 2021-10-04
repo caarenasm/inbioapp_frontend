c1cf7252946214188e8ed88ae47d376738922a19
Yoser Perez <yoserp1@gmail.com>
2021-10-04 13:25:16 -0500

    vista planes

---
+++ src/app/interfaces/plan.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface Plans {
+    id: number;
+    titulo: string;
+    descripcion: string;
+    imagen_url: string;
+    precio: number;
+    texto_tiempo: string;
+    texto_anual: string;
+  }
